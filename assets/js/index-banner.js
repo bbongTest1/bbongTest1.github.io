8aaa4b0c451a8a5613956e19058e246252ba05ca
BBong119 <59595485+BBong119@users.noreply.github.com>
2020-10-27 11:05:31 +0800

    aa

--- assets/js/index-banner.js
+++ assets/js/index-banner.js
@@ -275,3 +275,24 @@ function UsefulRecord(isUseful) {
         feedbackTag.innerHTML = "Thanks!";
     }
 }
+
+function UrlReplace()
+{
+    var curRelativeUrl = (document.URL.split(document.domain)).pop();
+    var allHref = $(".content")[0].find("href");
+
+    var tmpExp = new RegExp(/-v[0-9]+.*\//g)
+    var searchAry = tmpExp.exex(curRelativeUrl);
+    if (searchAry != null)
+    {
+        var needFindStr = curRelativeUrl.split(searchAry[0])[0] + "/";
+        var needReplaceStr = curRelativeUrl.split(searchAry[0])[0];
+
+        var allHref = $(".content")[0].find("href");
+    
+        for (var i = 0; i < allHref.length; i++)
+        {
+            
+        }
+    }
+}
